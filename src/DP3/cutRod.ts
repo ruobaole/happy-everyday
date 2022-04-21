78f1302442c86ebdd05c0db527a76eb779c1f518
ruobaole <daisyrpanglive@live.com>
2022-04-21 12:16:26 +0800

    review practice DP - unbounded knapsack

--- src/DP3/cutRod.ts
+++ src/DP3/cutRod.ts
@@ -120,3 +120,72 @@ export function rodCutting2(
   }
   return selected
 }
+
+//--- r2 ---//
+//
+// Problem: find the selection with the max profit, and return the selected items
+// The problem is very much similar to unbounded-knapsack, but not the same.
+// The difference is that, in this problem, a rop with length l can be selected more than
+//  once.
+// Hence, in processing each rop with length lengths[i], when branching between if including in
+//  the item or not, if including the item, we can have it in the selection more than once --
+// DP[i][l - lengths[i]] instead of DP[i - 1][l - lengths[i]]
+// How to find the selection?
+// from the final matrix's bottom-right corner; each cell's value can be coming from either the
+// two ways --
+// 1) its top cell -- meaning that the item represented by the row is not selected
+// 2) a cell in the same row to the left of it (col = curCol - lengths[i - 1]) -- meaning that the item
+//  is selected
+// - thus, starting from the bottom-right corner, we first going up until the top cell's value !== current cell's value;
+// - going left, jump to the col - curCol - lengths[i - 1]
+// - including the item represented by the current row in the selection
+// - continue the process until we've reached the 1st row
+//
+// Time: O(M * N) -- M is the number of possible rod-length cut; N is the total rod's length;
+// Space: O(M * N)
+
+export function rodCutting_r1(
+  lengths: number[],
+  profits: number[],
+  n: number
+): number[] {
+  const selection: number[] = []
+  if (n <= 0 || lengths.length === 0 || lengths.length !== profits.length) {
+    return selection
+  }
+
+  // DP[i][l] - the max profit to cut the rod with total length l with possible lengths
+  //  of the first i items in the given array
+  const DP = new Array(lengths.length + 1)
+    .fill(0)
+    .map(() => new Array(n + 1).fill(0))
+  for (let i = 1; i <= lengths.length; i += 1) {
+    for (let l = 1; l <= n; l += 1) {
+      // 1. try including in the i-1 th item
+      let profit1 = 0
+      if (lengths[i - 1] <= l) {
+        profit1 = profits[i - 1] + DP[i][l - lengths[i - 1]]
+      }
+      // 2. try not including in the i-1 th item
+      const profit2 = DP[i - 1][l]
+      DP[i][l] = Math.max(profit1, profit2)
+    }
+  }
+
+  // find the selection
+  const curProfit = DP[lengths.length][n]
+  let curRow = lengths.length
+  let curCol = n
+  while (curRow > 0) {
+    // 1. going up until the top cell's value is different
+    while (curRow > 0 && curProfit === DP[curRow - 1][curCol]) {
+      curRow -= 1
+    }
+    // 2. skip to left
+    if (curRow > 0) {
+      curCol -= lengths[curRow - 1]
+      selection.push(curRow)
+    }
+  }
+  return selection
+}
--- src/DP3/minCoinChange.ts
+++ src/DP3/minCoinChange.ts
@@ -46,3 +46,58 @@ export function minCoinChange(denominations: number[], total: number): number {
   }
   return DP[N - 1][total]
 }
+
+//--- r2 ---//
+//
+// DP[i][m] -- min number of coins needed to make up a total amount of m using the first i
+//  denominations; (-1 if the amount cannot be made)
+// Deduction:
+// for every DP[i][m], we branching between including the i-1 th denomination in the final set or not;
+// 1. if denominations[i-1] <= m, we can try including AT LEAST one of the denomination
+//  DP[i][m - deno[i-1]] === -1 ? -1 : DP[i][m - deno[i-1]] + 1
+// 2. try not including any of the deno
+//  DP[i-1][m]
+// if any of the 2 cases gives -1, the answer is the non-negative one
+// else, min(case1, case2)
+// Base Case:
+// - DP[i][0] = 0 because we can always get amount of 0 using no coins at all
+//
+// Time: O(N * M)
+// Space: O(N * M) -- although can be saved up to O(M) by reusing 2 rows as DP[i] and DP[i-1]
+
+export function minCoinChange_r1(
+  denominations: number[],
+  total: number
+): number {
+  if (denominations.length === 0) {
+    return total === 0 ? 0 : -1
+  }
+  if (total <= 0) {
+    return -1
+  }
+
+  const DP = new Array(denominations.length + 1)
+    .fill(-1)
+    .map(() => new Array(total + 1).fill(-1))
+  for (let i = 0; i <= denominations.length; i += 1) {
+    DP[i][0] = 0
+  }
+
+  for (let i = 1; i <= denominations.length; i += 1) {
+    for (let m = 1; m <= total; m += 1) {
+      // 1. try including at least one of the cur denomination
+      let cnt1 = -1
+      if (denominations[i - 1] <= m && DP[i][m - denominations[i - 1]] >= 0) {
+        cnt1 = DP[i][m - denominations[i - 1]] + 1
+      }
+      // 2. try not including any of the cur denomination
+      const cnt2 = DP[i - 1][m]
+      if (cnt1 >= 0 && cnt2 >= 0) {
+        DP[i][m] = Math.min(cnt1, cnt2)
+      } else {
+        DP[i][m] = cnt1 >= 0 ? cnt1 : cnt2
+      }
+    }
+  }
+  return DP[denominations.length][total]
+}
